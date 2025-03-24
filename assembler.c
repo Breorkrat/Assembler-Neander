80840832e7c46bff7c81af4f2484a1f84d2fac20
Breorkrat <mkrzesinskidasilva@gmail.com>
2025-03-24 16:07:38 -0300

    Reformulação do código

--- assembler.c
+++ assembler.c
@@ -10,10 +10,11 @@ int hash(unsigned char *str)
 }
 
 // Skip indica onde o endereço começa, 3 para funções como JZ e 4 para STA...
-unsigned char address(char *str, char skip)
+unsigned short int address(char *str, char skip)
 {
-  unsigned char valor;
-  sscanf(str + skip, "%hhx", &valor);
+  unsigned short int valor;
+  sscanf(str + skip, "%hx", &valor);
+  printf("%d\n", valor);
   return valor;
 }
 
@@ -36,17 +37,17 @@ int main()
   saida = fopen("out.mem", "w");
 
   char raw[mem] = {};
-  unsigned char final[mem] = {0x03, 0x4E, 0x44, 0x52};
+  unsigned short int final[mem] = {0x4E03, 0x5244};
   unsigned char desvio[mem] = {0};
 
 
-  int bytes = 4;
+  int bytes = 2;
   for (char linha = 1; fgets(raw, mem, entrada); linha ++)
   {
     //printf("Escrevendo no byte %X\n", bytes);
     unsigned char posDesvio = temDesvio(desvio, linha);
     if(posDesvio != 0) {
-      final[posDesvio] = bytes;
+      final[posDesvio] = bytes - 4;
       //printf("Final[%X] vai para %X\n", posDesvio, bytes);
       //printf("Desvio: %X, Destino: %X", posDesvio, posDesvio-bytes);
 
@@ -60,79 +61,78 @@ int main()
       {
       case 93: // NOP
         final[bytes] = 0x00;
-        bytes+=2;
         break;
 
       case 98: // STA
         final[bytes] = 0x10;
-        final[bytes + 2] = address(raw, 4);
-        bytes += 4;
+        final[bytes + 1] = address(raw, 4);
+        bytes += 2;
         break;
 
       case 61: // LDA
         final[bytes] = 0x20;
-        final[bytes + 2] = address(raw, 4);
-        bytes += 4;
+        final[bytes + 1] = address(raw, 4);
+        bytes += 2;
         break;
 
       case 31: // ADD
         final[bytes] = 0x30;
-        final[bytes + 2] = address(raw, 4);
-        bytes += 4;
+        final[bytes + 1] = address(raw, 4);
+        bytes += 2;
         break;
 
       case 51: // OR
         final[bytes] = 0x40;
         final[bytes + 2] = address(raw, 3);
-        bytes += 4;
+        bytes += 2;
         break;
 
       case 41: // AND
         final[bytes] = 0x50;
-        final[bytes + 2] = address(raw, 4);
-        bytes += 4;
+        final[bytes + 1] = address(raw, 4);
+        bytes += 2;
         break;
 
       case 97: // NOT
         final[bytes] = 0x60;
-        bytes+=2;
+        bytes+=1;
         break;
 
       case 79: // JMP
         final[bytes] = 0x80;
 
         desvio[bytes] = 0x80;
-        desvio[bytes+2] = address(raw, 4);
+        desvio[bytes+1] = address(raw, 4);
         //printf("JMP para %X\n", desvio[bytes+3]);
 
-        bytes += 4;
+        bytes += 2;
         break;
 
       case 32: // JN
         final[bytes] = 0x90;
-        final[bytes + 2] = address(raw, 3);
+        final[bytes + 1] = address(raw, 3);
 
         //desvio[bytes+2][0] = bytes+2;
         //desvio[bytes + 2][1] = address(raw, 4);
         //printf("PosDesvio: %X, para %X\n", bytes+2, desvio[bytes+2][1]);
 
-        bytes += 4;
+        bytes += 2;
         break;
 
       case 44: // JZ
         final[bytes] = 0xA0;
-        final[bytes + 2] = address(raw, 3);
+        final[bytes + 1] = address(raw, 3);
 
         //desvio[bytes+2][0] = bytes+2;
         //desvio[bytes + 2][1] = address(raw, 4);
         //printf("PosDesvio: %X, para %X\n", bytes+2, desvio[bytes+2][1]);
 
-        bytes += 4;
+        bytes += 2;
         break;
 
       case 76: // HLT
         final[bytes] = 0xF0;
-        bytes+=2;
+        bytes+=1;
         break;
       }
     }
---
+++ main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_MEM 512
+
+int hash(char *str)
+{
+  int hash = (str[0] * 3 + str[1] + str[2]) % 100;
+  return hash;
+}
+
+char address(char *str, int start){
+    int value = atoi(str+start);
+    return (char)value;
+}
+
+int checarDesvio(char linhas[MAX_MEM], char linha, int max){
+    for (int i = 0; i < max; i++){
+	if (linhas[i] == linha){
+	    return i;
+	}
+    }
+    return -1;
+}
+
+int main()
+{
+  // https://www.inf.ufrgs.br/~johann/neander2024/
+
+  FILE *entrada, *saida;
+  entrada = fopen("src.txt", "r");
+  saida = fopen("out.mem", "w");
+
+  char headers[] = {0x03, 0x4E, 0x44, 0x52};
+  char raw[MAX_MEM];
+  char mem[MAX_MEM] = {0};
+  char desvio[2][MAX_MEM] = {0};
+  int quantDesvio = 0;
+  int bytes = 0;
+
+  for (char linha = 1; fgets(raw, MAX_MEM, entrada); linha ++)
+  {
+    int temDesvio = checarDesvio(desvio[0], linha, quantDesvio);
+    if (temDesvio != -1){
+	printf("Linha: %d | Desvio para %d\n", linha, desvio[1][temDesvio]);
+	mem[desvio[1][temDesvio]] = bytes;
+    }
+
+    if (raw[0] >= 'A' && raw[0] <= 'Z')
+    {
+      char key[] = {raw[0], raw[1], raw[2]};
+      switch (hash(key))
+      {
+      case 93: // NOP
+        mem[bytes] = 0x00;
+        break;
+
+      case 98: // STA
+        mem[bytes] = 0x10;
+        bytes += 2;
+        mem[bytes] = address(raw, 4);
+        break;
+
+      case 61: // LDA
+        mem[bytes] = 0x20;
+        bytes += 2;
+        mem[bytes] = address(raw, 4);
+        break;
+
+      case 31: // ADD
+        mem[bytes] = 0x30;
+        bytes += 2;
+        mem[bytes] = address(raw, 4);
+        break;
+
+      case 51: // OR
+        mem[bytes] = 0x40;
+        bytes += 2;
+        mem[bytes] = address(raw, 3);
+        break;
+
+      case 41: // AND
+        mem[bytes] = 0x50;
+        bytes += 2;
+        mem[bytes] = address(raw, 4);
+        break;
+
+      case 97: // NOT
+        mem[bytes] = 0x60;
+        break;
+
+      case 79: // JMP
+        mem[bytes] = 0x80;
+	desvio[0][quantDesvio] = address(raw, 4); // Linha que o desvio procura
+	bytes += 2;
+	desvio[1][quantDesvio] = bytes;		  // Endereço para voltar
+	quantDesvio++;
+        break;
+
+      case 32: // JN
+        mem[bytes] = 0x90;
+        bytes += 2;
+        mem[bytes] = address(raw, 3);
+        break;
+
+      case 44: // JZ
+        mem[bytes] = 0xA0;
+        bytes += 2;
+        mem[bytes] = address(raw, 3);
+        break;
+
+      case 76: // HLT
+        mem[bytes] = 0xF0;
+        break;
+      }
+      bytes += 2;
+    }
+
+    if (raw[0] == '*') {
+      mem[address(raw, 1)*2+4] = address(raw, 3);
+    }
+  }
+
+
+  fwrite(headers, 1, sizeof(headers), saida);
+  fwrite(mem, 1, MAX_MEM, saida);
+
+
+  fclose(entrada);
+  fclose(saida);
+
+  return 0;
+}
+
